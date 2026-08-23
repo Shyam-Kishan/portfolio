a5063fffef98aaeb4e7495ef52c0bc20bfca6a1d
Shyam Kishan <shyamkishan109@gmail.com>
2026-08-23 14:00:01 -0700

    Initial commit: navbar, hero, and project sections

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,25 @@
+import Link from "next/link";
+
+export default function Navbar() {
+  return (
+    <nav className="flex sticky top-0 z-50 justify-between 
+                    items-center bg-neutral-900 px-5 py-5">
+        <ul>
+            <li>
+                <Link className="underline hover:text-neutral-400 transition-colors duration-500" 
+                href="/">Shyam Kishan</Link>
+            </li>
+        </ul>
+        <ul className="flex items-center gap-4">
+            <li>
+                <Link className="underline hover:text-neutral-400 transition-colors duration-500"
+                href="/#projects">Projects</Link>
+            </li>
+            <li>
+                <Link className="underline hover:text-neutral-400 transition-colors duration-500"
+                href="/#about">About</Link>
+            </li>
+        </ul>
+    </nav>
+  );
+}
